14c31693c6556cc18b17bcaaaf9a0187a865d9ee
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-05-25 22:08:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'preserve',
+    vueIndentScriptAndStyle: false,
+    singleAttributePerLine: false,
+    printWidth: 150,
+    htmlWhitespaceSensitivity: 'ignore',
+}
+
+export default config
